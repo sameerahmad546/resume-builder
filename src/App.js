c94243b5cd10d469a52ec15c76be17212924f692
Sameer Ahmad <sameerah546@gmail.com>
2023-10-29 22:16:53 +0500

    initial setup with tailwind

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='bg-blue-700 text-white p-4'>
+      this is tailwind text
+    </div>
+  );
+}
+
+export default App;
